4c5b23f984e90622e6a19287a450dd3e95199f4b
Eduardo <eduardoandres894@gmail.com>
2024-07-29 19:46:05 -0400

    Entrega 1

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-var nombre = "Eduardo Peña";
+let nose = "a"
 
-document.write(nombre)
+let favoritos = true
+
+let canciones = 10
+
+if (favoritos){
+    datos.innerHTML += "Esta en favoritos"
+}else{
+    datos.innerHTML += "No esta en favoritos"
+}
+
+for(var i = 0; i <= canciones; i++)
+    datos.innerHTML += "<p>cancion"+i+"</p>"
